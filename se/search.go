08500ff073fc1e36de4806ac9dee7538244a135f
fengwei <fengwei@rightpaddle.com>
2020-04-21 20:40:19 +0800

    structs

--- se/search.go
+++ se/search.go
@@ -5,6 +5,7 @@
 package se
 
 import (
+	"github.com/longlivefelix/tiny_search/se/structs/block"
 	"github.com/longlivefelix/tiny_search/se/structs/group"
 )
 
@@ -12,6 +13,19 @@ type Search struct {
 	Instance *group.Group
 }
 
-func NewGroup(groupName string) *Search{
-	return &Search{}
+var searchInstance map[string]*Search
+
+func NewGroup(groupName string) *Search {
+	if instance, ok := searchInstance[groupName]; ok {
+		return instance
+	}
+	searchInstance[groupName] = &Search{
+		Instance: &group.Group{
+			GroupName:           groupName,
+			BlockList:           []block.BlockInfo{},
+			BlockListCountLimit: 64,               // 10m*64
+			BlockSizeLimit:      1024 * 1024 * 10, // 10m
+		},
+	}
+	return searchInstance[groupName]
 }
